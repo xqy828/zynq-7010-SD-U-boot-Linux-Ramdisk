6853db74869b8b070a8d197b1936f81abb66b92b
qiyang.xing <snown@live.cn>
2023-11-06 21:54:38 +0800

    add ebaz4205 defconfig and compile script

---
+++ linux-xlnx-xilinx-v2020.2/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+start_time=$(date +%s)
+export ARCH=arm
+export CROSS_COMPILE=arm-xilinx-linux-gnueabi-
+export PATH=$PATH:/opt/Petalinux/petalinux-v2015.4-final/tools/linux-i386/arm-xilinx-linux-gnueabi/bin
+echo make distclen
+make distclean
+echo make ebaz4205_defconfig
+make ebaz4205_defconfig
+#make menuconfig
+echo make uImage
+make uImage UIMAGE_LOADADDR=0x8000 -j8
+end_time=$(date +%s)
+cost_time=$[ $end_time-$start_time ]
+echo "build kernel time is $(($cost_time/60))min $(($cost_time%60))s"
+
+
+
